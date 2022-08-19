c6337e60ec273be787cc05e6ef9702beea0db648
github_singll <huanyuesingll@outlook.com>
2022-08-19 15:22:12 +0800

    sveltekit init

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,10 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	},
-
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			}
-		})
-	]
+	}
 };
 
 export default config;
